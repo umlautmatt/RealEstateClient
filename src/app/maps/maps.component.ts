c657ae5727ad8f9ea80fc98e369a7c1ec0f82472
zringis <zringis@gmail.com>
2019-06-06 14:00:29 -0400

    Added AutoFill

--- src/app/maps/maps.component.ts
+++ src/app/maps/maps.component.ts
@@ -1,12 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {Title} from '@angular/platform-browser';
+import {Location, Appearance} from '@angular-material-extensions/google-maps-autocomplete';
+import { ViewEncapsulation } from '@angular/core';
+import PlaceResult = google.maps.places.PlaceResult;
+import { BrowserModule } from '@angular/platform-browser';
+import { MatGoogleMapsAutocompleteModule } from '@angular-material-extensions/google-maps-autocomplete';
 
 @Component({
   selector: 'app-map',
   templateUrl: 'maps.component.html',
   styleUrls: ['maps.component.css'],
+  encapsulation: ViewEncapsulation.None,
 })
-export class MapsComponent {
-  title: string = 'My first AGM project';
-  lat: number = 51.678418;
-  lng: number = 7.809007;
+export class MapsComponent implements OnInit {
+
+  public appearance = Appearance;
+  public zoom: number;
+  public latitude: number;
+  public longitude: number;
+  public selectedAddress: PlaceResult;
+
+  constructor(private titleService: Title) {
+  }
+
+  ngOnInit() {
+    this.titleService.setTitle('Home | @angular-material-extensions/google-maps-autocomplete');
+
+    this.zoom = 10;
+    this.latitude = 52.520008;
+    this.longitude = 13.404954;
+
+    this.setCurrentPosition();
+
+  }
+
+  private setCurrentPosition() {
+    if ('geolocation' in navigator) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        this.latitude = position.coords.latitude;
+        this.longitude = position.coords.longitude;
+        this.zoom = 12;
+      });
+    }
+  }
+
+  onAutocompleteSelected(result: PlaceResult) {
+    console.log('onAutocompleteSelected: ', result);
+  }
+
+  onLocationSelected(location: Location) {
+    console.log('onLocationSelected: ', location);
+    this.latitude = location.latitude;
+    this.longitude = location.longitude;
+  }
 }
