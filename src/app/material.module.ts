f37c9b90cf4a4620734316c6256825d3f1e2e8c9
umlautmatt <mahawley24@gmail.com>
2019-06-06 14:15:45 -0400

    added custom material module

---
+++ src/app/material.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatInputModule, 
+  MatButtonModule, 
+  MatToolbarModule, 
+  MatFormFieldModule, 
+  MatTableModule } from '@angular/material';
+
+@NgModule({
+declarations: [],
+imports: [
+CommonModule,
+MatToolbarModule,
+MatInputModule,
+MatButtonModule,
+MatFormFieldModule,
+
+MatTableModule
+],
+exports: [
+
+MatToolbarModule,
+MatInputModule,   
+MatButtonModule,
+MatFormFieldModule,
+MatTableModule
+]
+})
+export class MaterialModule { }
