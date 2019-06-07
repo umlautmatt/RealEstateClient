2ab11b112dcc904da1e05e6588d0ad946378a5cc
AaronGitsHub <aaronwood5@mac.com>
2019-06-06 21:24:52 -0400

    RentServiceAPICall_Draft

--- src/app/Services/rent.service.ts
+++ src/app/Services/rent.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Rent } from '../Models/Rent';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RentService {
+apiUrl = 'https://realestatemanagerwebapi20190606115209.azurewebsites.net/api/Rent/';
+  constructor(private _http: HttpClient) { }
 
-  constructor() { }
+  getRent() {
+    return this._http.get<Rent[]>(this.apiUrl);
+  }
+
 }
