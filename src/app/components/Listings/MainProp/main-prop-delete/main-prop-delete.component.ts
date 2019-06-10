a74356fe306cddd2c639cacff8762f12b645fbe2
acordray79 <acordray79@gmail.com>
2019-06-10 08:18:13 -0400

    mainprop delete

--- src/app/components/Listings/MainProp/main-prop-delete/main-prop-delete.component.ts
+++ src/app/components/Listings/MainProp/main-prop-delete/main-prop-delete.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MainPropService } from '../../../../Services/main-prop.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { MainProp } from '../../../../Models/MainProp';
 
 @Component({
   selector: 'app-main-prop-delete',
@@ -7,9 +10,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainPropDeleteComponent implements OnInit {
 
-  constructor() { }
+  mainProp: MainProp;
+
+  constructor(private _mainPropService: MainPropService, private _ar: ActivatedRoute, private _router: Router) {
+    this._ar.paramMap.subscribe(p => {
+      this._mainPropService.getMainProp(p.get('id')).subscribe((singleMainProp: MainProp) => {
+        this.mainProp = singleMainProp;
+      });
+    });
+   }
 
   ngOnInit() {
+  }
+
+  onDelete() {
+    this._mainPropService.deleteMainProp(this.mainProp.RealEstatePropertyId).subscribe(() => {
+      this._router.navigate(['/mainprop']);
+    });
   }
 
 }
