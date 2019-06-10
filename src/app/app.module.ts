2ccbc1b5d387e5cbdc6dc6c4a445e89c061f9c3a
zringis <zringis@gmail.com>
2019-06-10 09:10:56 -0400

    Formatting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,14 +8,15 @@ import { MatGoogleMapsAutocompleteModule } from '@angular-material-extensions/go
 import { AgmCoreModule } from '@agm/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
-import { MatInputModule, 
-          MatButtonModule, 
-          MatToolbarModule, 
-          MatFormFieldModule, 
-          MatTableModule,
-          MatCardModule,
-          MatDialogModule
-        } from '@angular/material';
+import {
+  MatInputModule,
+  MatButtonModule,
+  MatToolbarModule,
+  MatFormFieldModule,
+  MatTableModule,
+  MatCardModule,
+  MatDialogModule
+} from '@angular/material';
 
 
 
@@ -82,7 +83,7 @@ import { BuyService } from './Services/buy.service';
     ReactiveFormsModule,
     BrowserAnimationsModule,
     HttpClientModule,
-    
+
     //Material Modules
     MatToolbarModule,
     MatInputModule,
