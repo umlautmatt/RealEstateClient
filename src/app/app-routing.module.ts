7b0641523f2c7ef40f19ffa7a7f8ea8a15c3ab93
umlautmatt <mahawley24@gmail.com>
2019-06-10 15:28:09 -0400

    app routing update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,22 +4,38 @@ import { HomeComponent } from './components/Home/home/home.component';
 import { RegisterComponent } from './components/Account/register/register.component';
 import { LoginComponent } from './components/Account/login/login.component';
 import { BuyIndexComponent } from './components/Listings/Buy/buy-index/buy-index.component';
-import { RentIndexComponent } from './components/Listings/Rent/rent-index/rent-index.component';
 import { BuyDetailsComponent } from './components/Listings/Buy/buy-details/buy-details.component';
+import { RentIndexComponent } from './components/Listings/Rent/rent-index/rent-index.component';
+import { RentDetailsComponent } from './components/Listings/Rent/rent-details/rent-details.component';
 import { MainPropIndexComponent } from './components/Listings/MainProp/main-prop-index/main-prop-index.component';
 import { MainPropCreateComponent } from './components/Listings/MainProp/main-prop-create/main-prop-create.component';
+import { MainPropUpdateComponent } from './components/Listings/MainProp/main-prop-update/main-prop-update.component';
+import { MainPropDetailsComponent } from './components/Listings/MainProp/main-prop-details/main-prop-details.component';
+import { MainPropDeleteComponent } from './components/Listings/MainProp/main-prop-delete/main-prop-delete.component';
 
 const routes: Routes = [
   {path: 'Home', component: HomeComponent},
   {path: 'Login', component: LoginComponent},
   {path: 'Register', component: RegisterComponent},
-  {path: 'ForSaleList', component: BuyIndexComponent},
-  {path: 'ForRentList', component: RentIndexComponent},
+
+  {path: 'ForSale',  children: [
+    {path: 'List', component: BuyIndexComponent},
+    {path: 'Details/:id', component: BuyDetailsComponent},
+  ]},
+  
+  {path: 'ForRent', children: [
+    {path: 'List', component: RentIndexComponent},
+    {path: 'Details/:id', component: RentDetailsComponent},
+  ]},
+  
   {path: 'MainProp', children: [
-  {path: '', component: MainPropIndexComponent },
-  {path: 'Create', component: MainPropCreateComponent }
-    ]},
-  {path: "ForSaleDetail/:id", component: BuyDetailsComponent},
+    {path: '', component: MainPropIndexComponent },
+    {path: 'Create', component: MainPropCreateComponent },
+    {path: 'Update/:id', component: MainPropUpdateComponent },
+    {path: 'Detail/:id', component: MainPropDetailsComponent },
+    {path: 'Delete/:id', component: MainPropDeleteComponent }
+      ]},
+  
   {path: "**", component: RegisterComponent}
 ];
 
