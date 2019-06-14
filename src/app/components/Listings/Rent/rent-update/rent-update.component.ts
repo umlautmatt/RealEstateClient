3c570c79a97078c5b5e0d620955699e3562558a1
acordray79 <acordray79@gmail.com>
2019-06-14 15:29:01 -0400

    fixed rent update

--- src/app/components/Listings/Rent/rent-update/rent-update.component.ts
+++ src/app/components/Listings/Rent/rent-update/rent-update.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, FormControl } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { fromEventPattern } from 'rxjs';
-import { Rent } from '../../../../Models/Rent';
+import { RentProp } from '../../../../Models/RentProp';
 import { RentService } from '../../../../Services/rent.service';
 
 @Component({
@@ -20,7 +20,7 @@ export class RentUpdateComponent implements OnInit {
     this.isActive = newValue ? true : false
   }
 
-  rent: Rent;
+  rent: RentProp;
 
   editRentForm: FormGroup;
 
@@ -29,7 +29,7 @@ export class RentUpdateComponent implements OnInit {
               private _ar: ActivatedRoute,
               private _router: Router) {
                 this._ar.paramMap.subscribe(p => {
-                  this._rentService.getRent(p.get('id')).subscribe((singleRent: Rent) => {
+                  this._rentService.getRent(p.get('id')).subscribe((singleRent: RentProp) => {
                     this.rent = singleRent;
                     this.createForm();
                   });
@@ -47,26 +47,55 @@ export class RentUpdateComponent implements OnInit {
       Description: new FormControl(this.rent.Description),
       UtilitiesIncluded: new FormControl(this.rent.UtilitiesIncluded),
       PetsAllowed: new FormControl(this.rent.PetsAllowed),
-      IsRentFavorite: new FormControl(this.rent.IsRentFavorite)
+      IsRentFavorite: new FormControl(this.rent.IsRentFavorite),
+
+      RealEstatePropertyId: new FormControl(this.rent.RealEstatePropertyId),
+      RealEstatePropertyName: new FormControl(this.rent.RealEstatePropertyName),
+      ImageLink: new FormControl(this.rent.ImageLink),
+      SquareFootage: new FormControl(this.rent.SquareFootage),
+      RealEstateAddress: new FormControl(this.rent.RealEstateAddress),
+      RealEstateCity: new FormControl(this.rent.RealEstateCity),
+      RealEstateState: new FormControl(this.rent.RealEstateState),
+      RealEstateZip: new FormControl(this.rent.RealEstateZip),
+      PropertyType: new FormControl(this.rent.PropertyType),
+      HasBasement: new FormControl(this.rent.HasBasement),
+      HasPool: new FormControl(this.rent.HasPool),
+      Bedroom: new FormControl(this.rent.Bedroom),
+      Bathroom: new FormControl(this.rent.Bathroom),
+      Stories: new FormControl(this.rent.Stories)
     });
   }
 
   onSubmit(form){
    
-    const updateRent: Rent = {
+    const updateRent: RentProp = {
       RentId: form.value.RentId,
       Available: form.value.Available,
       PricePerMonth: form.value.PricePerMonth,
       Description: form.value.Description,
       UtilitiesIncluded: form.value.UtilitiesIncluded,
       PetsAllowed: form.value.PetsAllowed,
-      IsRentFavorite: form.value.IsRentFavorite
+      IsRentFavorite: form.value.IsRentFavorite,
+      RealEstatePropertyId: form.value.RealEstatePropertyId,
+      RealEstatePropertyName: form.value.RealEstatePropertyName,
+      ImageLink: form.value.ImageLink,
+      SquareFootage: form.value.SquareFootage,
+      RealEstateAddress: form.value.RealEstateAddress,
+      RealEstateCity: form.value.RealEstateCity,
+      RealEstateState: form.value.RealEstateState,
+      RealEstateZip: form.value.RealEstateZip,
+      PropertyType: form.value.PropertyType,
+      HasBasement: form.value.HasBasement,
+      HasPool: form.value.HasPool,
+      Bedroom: form.value.Bedroom,
+      Bathroom: form.value.Bathroom,
+      Stories: form.value.Stories
       
     };
     
     
     this._rentService.updateRent(updateRent).subscribe(d => {
-      this._router.navigate(['/Rent']);
+      this._router.navigate(['/ForRent']);
     });
     
   }
